6aa3e3c7e1caab0ce486835ba05a91e2632ce720
Shireesha <bongaralashireesha@gmail.com>
2015-06-17 18:32:02 +0530

    Added user class to display user details in required format

---
+++ src/com/twu/biblioteca/User.java
@@ -0,0 +1,20 @@
+package com.twu.biblioteca;
+
+public class User {
+    String name;
+    String email;
+    int phone;
+   private final String libraryID;
+
+    public User(String name, String email, int phone, String libraryID) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.libraryID = libraryID;
+    }
+
+    @Override
+    public String toString() {
+        return "Name : " + name + " Email : " + email +  " Phone : " + phone + " LibraryID :" + libraryID;
+    }
+}
---
+++ test/com/twu/biblioteca/UserTest.java
@@ -0,0 +1,17 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class UserTest {
+    @Test
+    public void shouldDisplayUserInformationInRequiredFormat(){
+        User user = new User("name","email",1234676,"123-4567");
+
+        String actualUserDetails = "Name : name Email : email Phone : 1234676 LibraryID :123-4567";
+
+        assertThat(actualUserDetails,is(user.toString()));
+    }
+}
