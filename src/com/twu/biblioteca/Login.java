a748ebc2f0c2b4c6dd5ff7ddae78c5c7a794c4a8
Shireesha <bongaralashireesha@gmail.com>
2015-06-17 17:10:25 +0530

    Login should read LibraryID from user

--- src/com/twu/biblioteca/Login.java
+++ src/com/twu/biblioteca/Login.java
@@ -4,11 +4,15 @@ import static com.twu.biblioteca.Messages.*;
 
 public class Login {
     OutputHandler outputHandler;
+    InputHandler inputHandler;
 
-    Login(OutputHandler outputHandler){
-        this.outputHandler =outputHandler;
+    Login(OutputHandler outputHandler,InputHandler inputHandler){
+        this.outputHandler = outputHandler;
+        this.inputHandler = inputHandler;
     }
     public void getDetails() {
         outputHandler.display(ENTER_LIBRARY_ID);
+       String LibraryID =  inputHandler.readLine();
+
     }
 }
--- test/com/twu/biblioteca/LoginTest.java
+++ test/com/twu/biblioteca/LoginTest.java
@@ -3,6 +3,8 @@ package com.twu.biblioteca;
 import org.junit.Test;
 import org.mockito.Mock;
 
+import java.io.ByteArrayInputStream;
+
 import static com.twu.biblioteca.Messages.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -10,13 +12,26 @@ import static org.mockito.Mockito.verify;
 public class LoginTest {
     @Mock
     OutputHandler outputHandlerMock;
+    @Mock
+    InputHandler inputHandlerMock;
+
     @Test
     public void shouldPromptUserToInputLibraryId() {
         outputHandlerMock = mock(OutputHandler.class);
-        Login login = new Login(outputHandlerMock);
+        Login login = new Login(outputHandlerMock,inputHandlerMock);
 
         login.getDetails();
 
         verify(outputHandlerMock).display(ENTER_LIBRARY_ID);
+    }
+    @Test
+    public void shouldGetTheLibraryIDFromTheUser() {
+        outputHandlerMock = mock(OutputHandler.class);
+        inputHandlerMock = mock(InputHandler.class);
+        Login login = new Login(outputHandlerMock,inputHandlerMock);
+
+        login.getDetails();
+
+      verify(inputHandlerMock).readLine();
     }
 }
