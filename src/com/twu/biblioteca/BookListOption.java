3100d8f8f9562ee6a7e56b0cae2cc0e7d4539a23
Shireesha <bongaralashireesha@gmail.com>
2015-06-18 11:16:24 +0530

    Added bookListOption.java

---
+++ src/com/twu/biblioteca/BookListOption.java
@@ -0,0 +1,23 @@
+package com.twu.biblioteca;
+
+import static com.twu.biblioteca.Messages.ENTER_BOOK_NAME;
+import static com.twu.biblioteca.Messages.PROMPT_USER_FOR_CHECKOUT_BOOK;
+
+public class BookListOption {
+    OutputHandler outputHandler;
+    CheckOutItem checkOutItem;
+
+    BookListOption(OutputHandler outputHandler, CheckOutItem checkOutItem) {
+        this.checkOutItem = checkOutItem;
+        this.outputHandler = outputHandler;
+    }
+    public void displayBookList(Books books) {
+        outputHandler.display(books);
+        outputHandler.display(PROMPT_USER_FOR_CHECKOUT_BOOK);
+        if (checkOutItem.isInterestedToCheckOut()) {
+            outputHandler.display(ENTER_BOOK_NAME);
+            checkOutItem.checkOutBook();
+        }
+
+    }
+}
---
+++ test/com/twu/biblioteca/BookListOptionTest.java
@@ -0,0 +1,30 @@
+package com.twu.biblioteca;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.*;
+
+public class BookListOptionTest {
+  @Mock
+  OutputHandler outputHandlerMock;
+    @Mock
+    CheckOutItem checkOutItemMock;
+
+    @Before
+    public void setUp() {
+        outputHandlerMock = mock(OutputHandler.class);
+        checkOutItemMock = mock(CheckOutItem.class);
+    }
+    @Test
+    public void shouldDisplayListOfBooks() {
+        BookListOption bookListOption = new BookListOption(outputHandlerMock, checkOutItemMock);
+        Books books = new Books(new Library().getAvailableBookList());
+
+        bookListOption.displayBookList(books);
+
+        verify(outputHandlerMock).display(books);
+
+    }
+}
