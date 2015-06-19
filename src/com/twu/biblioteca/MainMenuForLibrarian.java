33290d0ff5fe42a186c2528711353155bbd033ba
Shireesha <bongaralashireesha@gmail.com>
2015-06-19 12:08:36 +0530

    MainMenuForLibrarian displays list of books when user presses one

--- src/com/twu/biblioteca/MainMenuForLibrarian.java
+++ src/com/twu/biblioteca/MainMenuForLibrarian.java
@@ -4,11 +4,25 @@ import static com.twu.biblioteca.Messages.*;
 
 public class MainMenuForLibrarian {
     OutputHandler outputHandler;
-    public MainMenuForLibrarian(OutputHandler outputHandler) {
+    InputHandler inputHandler;
+    BookListOption bookListOption;
+    Books books;
+
+    public MainMenuForLibrarian(OutputHandler outputHandler,InputHandler inputHandler,BookListOption bookListOption,Books books) {
         this.outputHandler = outputHandler;
+        this.inputHandler = inputHandler;
+        this.bookListOption = bookListOption;
+        this.books = books;
     }
 
     public void start() {
         outputHandler.display(MAIN_MENU_FOR_LIBRARIAN);
+        int choice = inputHandler.readInteger();
+
+        switch (choice) {
+            case 1:
+                bookListOption.performAction(books);
+                break;
+        }
     }
 }
--- test/com/twu/biblioteca/MainMenuForLibrarianTest.java
+++ test/com/twu/biblioteca/MainMenuForLibrarianTest.java
@@ -10,17 +10,36 @@ import static org.mockito.Mockito.mock;
 public class MainMenuForLibrarianTest {
     @Mock
     OutputHandler outputHandlerMock;
+    @Mock
+    InputHandler inputHandlerMock;
+    @Mock
+    Books books;
+    @Mock
+    BookListOption bookListOption;
     @Before
     public void setUp() {
         outputHandlerMock = mock(OutputHandler.class);
+        inputHandlerMock = mock(InputHandler.class);
+        bookListOption = mock(BookListOption.class);
+        books = mock(Books.class);
 
     }
     @Test
     public void shouldDisplayAllOptionsForUser() {
-        MainMenuForLibrarian mainMenuForLibrarian = new MainMenuForLibrarian(outputHandlerMock);
+        MainMenuForLibrarian mainMenuForLibrarian = new MainMenuForLibrarian(outputHandlerMock,inputHandlerMock,bookListOption,books);
 
         mainMenuForLibrarian.start();
 
         verify(outputHandlerMock, atLeast(1)).display(Messages.MAIN_MENU_FOR_LIBRARIAN);
+    }
+    @Test
+    public void shouldDisplayBookListWhenUserPressesOne() {
+        MainMenuForLibrarian mainMenuForLibrarian = new MainMenuForLibrarian(outputHandlerMock,inputHandlerMock,bookListOption,books);
+
+     when(inputHandlerMock.readInteger())
+             .thenReturn(1);
+        mainMenuForLibrarian.start();
+
+        verify(bookListOption,atLeast(1)).performAction(books);
     }
 }
